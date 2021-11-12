0bc5f0de28fa98a1381549023cb6d2da25891842
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2021-11-13 03:37:31 +0600

    implement post and delete api for order and user

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
-const ObjectId = require("mongodb").ObjectId;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,8 @@ async function run() {
         await client.connect();
         const database = client.db('TwoWheelsZoneDb');
         const productsCollection = database.collection('products');
+        const usersCollection = database.collection('users');
+        const ordersCollection = database.collection('orders');
 
 
         //GET API for all products
@@ -32,6 +34,77 @@ async function run() {
             const products = await cursor.toArray();
             res.json(products);
         });
+
+        //GET API for single product
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+        });
+
+        //POST API for add a product
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            // console.log(newProduct);
+            const result = await productsCollection.insertOne(newProduct);
+            res.json(result);
+        });
+
+        //POST API for users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        //GET API for user order
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.json(orders);
+        });
+
+        //POST API for ordered products
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+
+        });
+
+        //DELETE API for user order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        //DELETE API for delete a product
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
     finally {
         // await client.close();
